3695499e4786b5d4cd04b340babae0a41ea5f3cd
Roman Paltera <palteraromanjr@gmail.com>
2024-03-20 03:34:47 +0800

    implement useAPI

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
 <template>
     <div>
         <UButton to="/quiz/create">Create Quiz</UButton>
-        {{ helloMessage }}
+        <UButton @click="fetchData">Check</UButton>
+        <div v-if="helloMessage">{{ helloMessage }}</div>
     </div>
 </template>
 
 <script setup lang="ts">
-const { data: helloMessage, pending, error } = await useMyFetch<string>('/');
+const { $api } = useNuxtApp()
+
+const helloMessage = ref()
+
+const fetchData = async () => {
+    if (!helloMessage.value) {
+        const data = await $api<string>('/')
+
+        helloMessage.value = data
+    }    
+}
 </script>
